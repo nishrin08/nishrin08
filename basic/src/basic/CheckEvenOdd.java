d8f3d8967cc6393a35cfa517ebf241b55a346c63
thasl <thasl@192.168.1.39>
2021-06-06 07:29:29 +0530

    first commit uploading all files

---
+++ basic/src/basic/CheckEvenOdd.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class CheckEvenOdd {
+	public static void main(String[] args) {
+		int n = 20;
+		if(n % 2 == 0)
+			System.out.println("Entered number is Even");
+		else
+			System.out.println("Entered number is Odd");
+	}
+
+}
